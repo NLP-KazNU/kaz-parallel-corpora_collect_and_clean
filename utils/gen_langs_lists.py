ac701d5ae98070521d13a1ab6954583b608188dd
Zhandos Zhumanov <jk-jdogg@yandex.ru>
2019-03-11 23:03:44 +0600

    Changged script from generating 3 files to generating 2 files

--- utils/gen_langs_lists.py
+++ utils/gen_langs_lists.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# script generates 3 lists with urls that differ only with language indication -
+# script generates 2 lists with urls that differ only with language indication -
 # /kz/, /ru/, /en/
 #
 # input - *.json file
@@ -12,20 +12,23 @@
 #       {"url": "https://..."}
 #   ]
 #
-# output - 3 text files: <site>_urls_kz.txt, <site>_urls_ru.txt, <site>_urls_en.txt
+# output - 2 text files: first - for the language used in input file, second - requested one
+# (two of the following: <site>_urls_kz.txt, <site>_urls_ru.txt, <site>_urls_en.txt)
 
 # docstring for docopt
 """gen_langs_lists.py
 
 Usage:
-    gen_langs_lists.py (-i | --input) <file_with_urls>
+    gen_langs_lists.py (-i | --input) <file_with_urls> --lang_from <lang_from> --lang_to <lang_to>
     gen_langs_lists.py (-h | --help)
     gen_langs_lists.py (-v | --version)
 
 Options:
-    -h --help       Show this screen.
-    -v --version    Show version.
     -i --input      path to a file with crawled urls (json)
+    --lang_from     language indication used in input file
+    --lang_to       requested second language indication
+    -v --version    Show version.
+    -h --help       Show this screen.
 """
 
 from docopt import docopt
@@ -46,8 +49,12 @@ def gen_list_and_file(
 
 
 def main():
-    # read json with urls into a list
+    # collect parameter values
     file_with_urls = arguments["<file_with_urls>"]
+    lang_from = arguments["<lang_from>"]
+    lang_to = arguments["<lang_to>"]
+
+    # read json with urls into a list
     with open(file_with_urls, "r") as json_data:
         urls_dict = json.load(json_data)
 
@@ -58,57 +65,16 @@ def main():
     # extract a site name from the first url
     site = re.search(pattern="\w+\.kz", string=urls[0]).group(0)
 
-    # generate url lists in all 3 languages
-    # save each list into its own file: <site>_urls_kz.txt, <site>_urls_ru.txt,
+    # generate url lists in 2 languages
+    # save each list into its own file: <site>_urls_kz.txt, <site>_urls_ru.txt, or
     # <site>_urls_en.txt
-    # if input urls contain indication for Russian (/ru/)
-    # generate /ru/, /kz/, /en/
-    if "/ru/" in urls[0]:
-        gen_list_and_file(url_list=urls, file_name=site + "_urls_ru.txt")
-        gen_list_and_file(
-            url_list=urls,
-            file_name=site + "_urls_kz.txt",
-            change_from="/ru/",
-            change_to="/kz/",
-        )
-        gen_list_and_file(
-            url_list=urls,
-            file_name=site + "_urls_en.txt",
-            change_from="/ru/",
-            change_to="/en/",
-        )
-    # if input urls contain indication for Kazakh (/kz/)
-    # generate /kz/, /ru/, /en/
-    elif "/kz/" in urls[0]:
-        gen_list_and_file(url_list=urls, file_name=site + "_urls_kz.txt")
-        gen_list_and_file(
-            url_list=urls,
-            file_name=site + "_urls_ru.txt",
-            change_from="/kz/",
-            change_to="/ru/",
-        )
-        gen_list_and_file(
-            url_list=urls,
-            file_name=site + "_urls_en.txt",
-            change_from="/kz/",
-            change_to="/en/",
-        )
-    # if input urls contain indication for English (/en/)
-    # generate /en/, /kz/, /ru/
-    elif "/en/" in urls[0]:
-        gen_list_and_file(url_list=urls, file_name=site + "_urls_en.txt")
-        gen_list_and_file(
-            url_list=urls,
-            file_name=site + "_urls_kz.txt",
-            change_from="/en/",
-            change_to="/kz/",
-        )
-        gen_list_and_file(
-            url_list=urls,
-            file_name=site + "_urls_ru.txt",
-            change_from="/en/",
-            change_to="/ru/",
-        )
+    gen_list_and_file(url_list=urls, file_name=site + "_urls_" + lang_from + ".txt")
+    gen_list_and_file(
+        url_list=urls,
+        file_name=site + "_urls_" + lang_to + ".txt",
+        change_from=lang_from,
+        change_to=lang_to,
+    )
 
 
 if __name__ == "__main__":
