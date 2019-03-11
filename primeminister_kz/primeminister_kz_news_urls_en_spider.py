7eba9a997698bd11d0bd8a8adb7a4a12a45c1ee2
Zhandos Zhumanov <jk-jdogg@yandex.ru>
2019-03-11 23:10:17 +0600

    Added spider for primeminister.kz en urls

---
+++ primeminister_kz/primeminister_kz_news_urls_en_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class primeminister_kz_news_urls_en(scrapy.Spider):
+    name = "primeminister_kz_news_urls_en"
+
+    def start_requests(self):
+        # общее количество страниц с новостями на сайте
+        total_news_pages = 1130
+        # массив для полного списка url
+        # приходится делать так, потому что в пагинации нет ссылок с меткой "next"
+        urls = []
+        for i in range(total_news_pages + 1):
+            urls.append("https://primeminister.kz/en/news/page/" + str(i))
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # response.css("a.item::attr(href)").get()
+        # вытаскиваем содержимое тегов <a class="item">
+        for url in response.css("a.item"):
+            # вытаскиваем из тегов <a class="item"> значение атрибута "href"
+            # собираем полный url
+            yield {"url": "https://primeminister.kz" + url.css("::attr(href)").get()}
